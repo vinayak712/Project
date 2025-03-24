7e467669899ff2d7d634dcc380725fbcc9b75d25
VinayakX <rasgotravinayak@gmail.com>
2025-03-25 00:18:16 +0530

    component Created

---
+++ frontend/src/components/ChatContainer.jsx
@@ -0,0 +1,4 @@
+function ChatContainer() {
+    
+}
+export default ChatContainer;
--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -1,7 +1,24 @@
+import { userChatstore } from "../store/userChatstore";
+import NoChat from '../components/NoChat'
+import ChatContainer from '../components/ChatContainer.jsx'
+import Sidebar from '../components/Sidebar.jsx'
+
 function Home() {
+    const { selectedUser } = userChatstore();
     return (
         <>
-        <h1>Home page</h1>
+            <div className="h-screen bg-base-200">
+                <div className=" flex items-center justify-center pt-20 px-4">
+                    <div className="bg-base-100 rounded-lg shadow-cl w-full max-w-6xl h-[calc(100vh-8rem)]">
+                    <div className="flex h-full rounded-lg overflow-hidden">
+            <Sidebar />
+
+           {!selectedUser ? <NoChat /> : <ChatContainer />}
+          </div>
+                    </div>
+
+                </div>
+        </div>
         </>
     );
 }
