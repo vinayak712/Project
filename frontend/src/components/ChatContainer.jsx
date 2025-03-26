d4921529e3c4295d406baa6c63554eef9062e9bf
VinayakX <rasgotravinayak@gmail.com>
2025-03-27 01:10:39 +0530

    creating chat container

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -1,10 +1,14 @@
 import { useState,useEffect } from "react";
 import { userChatstore } from "../store/userChatstore";
 import MessageInput from "./MessageInput";
+import { userAuthStore } from "../store/useAuthStore";
+import { formatMessageTime } from "../lib/utils";
+import defaultUserImage from "../assets/user.png";
 import ChatHeader from "./chatHeader";
 import { Loader } from "lucide-react";
 function ChatContainer() {
     const { messages, selectedUser, isMessageLoading, getMessages } = userChatstore();
+    const { authUser } = userAuthStore();
     useEffect(() => {
         if (selectedUser?._id) {
             getMessages(selectedUser._id);
@@ -23,7 +27,40 @@ function ChatContainer() {
         <>
             <div className="flex-1 flex flex-col overflow-auto">
                 < ChatHeader />
-                <p>messages..</p>
+         
+                <div className="flex flex-1 overflow-y-auto space-y-4 p-4">
+
+                    {messages.map((message) => (
+                        <div key={message._id}
+                            className={`chat ${message.senderId === authUser._id ? 'chat-end' : 'chat-start'}`}
+                            
+                        >
+                            <div className="size-10 rounded-full border">
+<img src={message.senderId===authUser._id?authUser.profilepic:selectedUser.profilepic || defaultUserImage } alt="profilepic" />
+                            </div>
+                            <div>
+                                <time className="text-sm opacity-50 ml-1">
+                                    {formatMessageTime(message.createdAt)}
+                                </time>
+                            </div>
+                            
+                            <div>
+                                {message.image && (
+                                    <img
+                                        src={message.image}
+                                        alt="Attachment"
+                                        className="sm:max-w-[200px] rounded-md mb-2"
+                                    />
+                                )}
+                                     {message.text && <p>{message.text}</p>}
+                            </div>
+            </div>
+        ))}
+
+                </div>
+
+
+
                 <MessageInput/>
  </div>
         </>
