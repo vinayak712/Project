bec880477c5931a25e8d9cb3613ace181de0afd5
VinayakX <rasgotravinayak@gmail.com>
2025-03-29 03:35:21 +0530

    t

--- frontend/src/components/chatHeader.jsx
+++ frontend/src/components/chatHeader.jsx
@@ -10,7 +10,7 @@ function chatHeader() {
         <>
             <div className="p-2.5 border-b border-base-300  ">
                 <div className="flex items-center justify-between ">
-                    <div className="flex items-center gap-3 fixed">
+                    <div className="flex items-center gap-3 ">
                         <div className="avatar">
                         <div className="size-10 rounded-full relative ">
                     <img src={selectedUser.profilepic|| defaultUserImage} alt={selectedUser.fullName} className="rounded-full "/>
